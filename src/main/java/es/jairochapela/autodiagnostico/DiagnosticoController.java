6d30d2964a6b5e356cca61ec36a254f4417c62bc
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-04-21 10:49:35 +0200

    Comentarios en el código.

--- src/main/java/es/jairochapela/autodiagnostico/DiagnosticoController.java
+++ src/main/java/es/jairochapela/autodiagnostico/DiagnosticoController.java
@@ -11,12 +11,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class DiagnosticoController implements WebMvcConfigurer {
 
 
+    /**
+     * Este método del controlador únicamente muestra la plantilla del formulario HTML.
+     * @param model
+     * @return
+     */
 	@GetMapping("/")
 	public String showForm(Model model) {
         model.addAttribute("paciente", new Paciente());
 		return "form";
     }
     
+    /**
+     * Este método recibe por POST los datos que el usuario introduce en el formulario,
+     * contenidos todos en el objeto paciente.
+     * @param paciente Los datos del paciente del formulario.
+     * @return
+     */
     @PostMapping("/")
     public String procesarDatos(@ModelAttribute Paciente paciente) {
         return "form";
