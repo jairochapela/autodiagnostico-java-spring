3a58fdb0fbf69418f86480fd36161cf3a09252c1
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-04-21 10:34:38 +0200

    Obtención de datos en el controlador.

--- src/main/java/es/jairochapela/autodiagnostico/DiagnosticoController.java
+++ src/main/java/es/jairochapela/autodiagnostico/DiagnosticoController.java
@@ -3,6 +3,8 @@ package es.jairochapela.autodiagnostico;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Controller
@@ -13,5 +15,10 @@ public class DiagnosticoController implements WebMvcConfigurer {
 	public String showForm(Model model) {
         model.addAttribute("paciente", new Paciente());
 		return "form";
-	}
+    }
+    
+    @PostMapping("/")
+    public String procesarDatos(@ModelAttribute Paciente paciente) {
+        return "form";
+    }
 }
