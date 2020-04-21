8c3db767026dfbe412bc1299acb120ae9acd0844
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-04-21 16:57:51 +0200

    Limpiando código.

--- src/main/java/es/jairochapela/autodiagnostico/AutodiagnosticoApplication.java
+++ src/main/java/es/jairochapela/autodiagnostico/AutodiagnosticoApplication.java
@@ -2,8 +2,7 @@ package es.jairochapela.autodiagnostico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * Esta es la clase principal, encargada de "levantar" el servicio mediante el
