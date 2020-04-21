3aa1feb86b6d40c70f8d13a0e867e325ab20e462
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-04-21 09:05:41 +0200

    Primer commit con el esqueleto de la aplicación.

---
+++ src/main/java/es/jairochapela/autodiagnostico/AutodiagnosticoApplication.java
@@ -0,0 +1,13 @@
+package es.jairochapela.autodiagnostico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutodiagnosticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutodiagnosticoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/jairochapela/autodiagnostico/AutodiagnosticoApplicationTests.java
@@ -0,0 +1,13 @@
+package es.jairochapela.autodiagnostico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutodiagnosticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
