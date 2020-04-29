e3756e3cb802aa97daa46014b560599a85a4141d
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-04-29 17:29:31 +0200

    Data seed.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,25 @@
+INSERT INTO comunidades VALUES 
+    (1, 'Andalucía'),
+    (2, 'Aragón'),
+    (3, 'Principado de Asturias'),
+    (4, 'Islas Baleares'),
+    (5, 'País Vasco'),
+    (6, 'Canarias'),
+    (7, 'Cantabria'),
+    (8, 'Castilla-La Mancha'),
+    (9, 'Castilla y León'),
+    (10, 'Cataluña'),
+    (11, 'Extremadura'),
+    (12, 'Galicia'),
+    (13, 'Comunidad de Madrid'),
+    (14, 'Región de Murcia'),
+    (15, 'Comunidad Foral de Navarra'),
+    (16, 'La Rioja'),
+    (17, 'Comunidad Valenciana'),
+    (18, 'Ceuta'),
+    (19, 'Melilla');
+
+INSERT INTO sintomas VALUES
+    (1, 'Fiebre'),
+    (2, 'Tos seca'),
+    (3, 'Difucultad respiratoria');
